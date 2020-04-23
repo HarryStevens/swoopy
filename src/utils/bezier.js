0cb2a82b1fb6f7124860a6c651e359ad117c20b5
HarryStevens <harryjosephstevens@gmail.com>
2020-04-23 18:07:24 -0400

    Move bezier functions to separate util file

---
+++ src/utils/bezier.js
@@ -0,0 +1,41 @@
+import { lineLength } from "./lineLength";
+
+// See https://math.stackexchange.com/a/1361717/659913
+export function quadBezier(a, b, c){
+  const i = t => {
+    const x = Math.pow((1 - t), 2) * a[0] + 2 * t * (1 - t) * b[0] + Math.pow(t, 2) * c[0];
+    const y = Math.pow((1 - t), 2) * a[1] + 2 * t * (1 - t) * b[1] + Math.pow(t, 2) * c[1];
+    return [x, y];
+  }
+
+  const l = lineLength([a, c]),
+        n = Math.floor(l),
+        o = [];
+
+  for (let j = 0; j <= n; j += 1){
+    o.push(i(j / n));
+  }
+
+  if (l !== n) o.push(c);
+  return o;
+}
+
+// See https://math.stackexchange.com/questions/26846/is-there-an-explicit-form-for-cubic-b%C3%A9zier-curves
+export function cubicBezier(a, b, c, d, p){
+  const i = t => {
+    const x = Math.pow((1 - t), 3) * a[0] + 3 * t * Math.pow((1 - t), 2) * b[0] + 3 * Math.pow(t, 2) * (1 - t) * c[0] + Math.pow(t, 3) * d[0];
+    const y = Math.pow((1 - t), 3) * a[1] + 3 * t * Math.pow((1 - t), 2) * b[1] + 3 * Math.pow(t, 2) * (1 - t) * c[1] + Math.pow(t, 3) * d[1];
+    return [x, y];
+  }
+
+  const l = lineLength([a, d]),
+        n = Math.floor(l),
+        o = [];
+
+  for (let j = 0; j <= n; j +=  1){
+    o.push(i(j / n));
+  }
+
+  if (l !== n) o.push(d);
+  return o;
+}
